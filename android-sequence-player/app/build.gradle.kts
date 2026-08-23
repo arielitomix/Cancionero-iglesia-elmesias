83c3aa83249c666548848396ecf28dcc5e9c4137
arielitomix <barreraoscar692@gmail.com>
2026-08-23 00:01:41 -0600

    Bump Sequence Player to 0.15

--- android-sequence-player/app/build.gradle.kts
+++ android-sequence-player/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.arielalvarez.sequenceplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.14.1"
+        versionCode = 16
+        versionName = "0.15"
     }
 
     compileOptions {
